e61e2a0313b6369843d3858f3c01c89ca874ae20
Rimunanda Ma'arif Parinduri <arimrimunanda15@gmail.com>
2023-03-26 14:36:04 +0700

    Menambahkan view binding

--- app/src/main/java/com/d3if3105/mobpro/Assesment01/MainActivity.kt
+++ app/src/main/java/com/d3if3105/mobpro/Assesment01/MainActivity.kt
@@ -2,10 +2,13 @@ package com.d3if3105.mobpro.Assesment01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.d3if3105.mobpro.Assesment01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
