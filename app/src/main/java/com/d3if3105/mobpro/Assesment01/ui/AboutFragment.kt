4998b6256b02baf116f1141792f3410ac4bacd3e
Rim <arimrimunanda15@gmail.com>
2023-05-20 11:53:18 +0700

    membuat navigasi dengan menu

---
+++ app/src/main/java/com/d3if3105/mobpro/Assesment01/ui/AboutFragment.kt
@@ -0,0 +1,6 @@
+package com.d3if3105.mobpro.Assesment01.ui
+
+import androidx.fragment.app.Fragment
+import com.d3if3105.mobpro.Assesment01.R
+
+class AboutFragment : Fragment(R.layout.fragment_about)
--- app/src/main/java/com/d3if3105/mobpro/Assesment01/ui/HitungFragment.kt
+++ app/src/main/java/com/d3if3105/mobpro/Assesment01/ui/HitungFragment.kt
@@ -1,14 +1,13 @@
 package com.d3if3105.mobpro.Assesment01.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.d3if3105.mobpro.Assesment01.R
 import com.d3if3105.mobpro.Assesment01.databinding.FragmentHitungBinding
 import com.d3if3105.mobpro.Assesment01.db.BangunDatarDb
@@ -23,6 +22,7 @@ class HitungFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHitungBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -54,5 +54,18 @@ class HitungFragment : Fragment() {
         viewModel.luas.observe(viewLifecycleOwner, Observer { luas ->
             binding.luasTextView.text = "%.0f".format(luas)
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
